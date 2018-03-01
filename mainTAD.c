f046f3e71b4a7e5913167529c4dd9e932c57c1f7
FelipeReksua <36967294+FelipeReksua@users.noreply.github.com>
2018-03-01 17:12:51 -0300

    Create mainTAD.c

---
+++ mainTAD.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "TAD.c"
+
+int main(){
+    //incluir();
+    incluir(3);
+    incluir(5);
+    incluir(7);
+    incluir(9);
+    imprime();
+    excluir(7);
+    imprime();
+    //alterar();
+    //consultar();
+    return 0;
+    }
